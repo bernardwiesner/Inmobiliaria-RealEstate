f6ee83de0d90a5c1572aed3d4510ed6b7cdb8f9e
linker71 <deamonlinker@yahoo.com>
2016-11-21 22:40:28 -0400

    Update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,7 @@
   <head>
     <title>Inmobi Itla</title>
     <link rel="stylesheet" href="<?php echo base_url('css/custom.css') ?>">
-
+    <link rel="icon" href="<?php echo base_url('favicon.gif')?>" type="image/gif">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
